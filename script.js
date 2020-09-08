d97e1b2265cbf4bcc99278082c4391bc0d9dcb3c
Glitch (hello-webpage) <none>
2020-09-08 02:14:50 +0000

    🍃🏠 Checkpoint ./index.html:8045597/726 ./script.js:8045597/272

---
+++ script.js
@@ -0,0 +1,3 @@
+function draw(title, subtitle, desc, color){
+  document.getElementById("preview").innerHTML = `<div style="background-color:lightblue;"><div class="container"><h1>${title}</h1></div></div>`;
+}
