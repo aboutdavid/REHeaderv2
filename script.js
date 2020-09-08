9fc2116e2b7be1ffc743be2ecc3f6736994840ab
Glitch (reheaderv2) <none>
2020-09-08 15:02:04 +0000

    ⛷👑 Checkpoint ./script.js:8045597/131 ./index.html:8045597/7599 ./.glitch-assets:8045597/354 ./form.html:8045597/763

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const urlParams = new URLSearchParams(window.location.search);
 function draw(image, title, subtitle, desc, color){
-  document.writeln(`<div style="background-color:${color};"><br><br>`);
+  document.writeln(`<div style="background-color:${color};line-height: 20px;"><br><br>`);
   // <img src="Error.src" onerror="this.style.display='none'"/>
-  document.writeln(`<center><img src="${image}" alt="Profile Picture" width="500" height="500" onerror="this.style.display='none'"></center>`)
+  document.writeln(`<center><img src="${image}" alt="Profile Picture" width="128" height="128" onerror="this.style.display='none'"></center>`)
   document.writeln(`<p class="desc">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${title}</p>`);
   document.writeln(`<p class="head">&nbsp;&nbsp;&nbsp;&nbsp;${subtitle}</p>`);
-  document.writeln(`<p style="font-weight:300;font-size:20px;"><center>&nbsp;&nbsp;&nbsp;&nbsp;${desc}</center></p>`);
+  document.writeln(`<p style="font-weight:300;font-size:20px;"><center>&nbsp;&nbsp;&nbsp;&nbsp;${desc}</center></p><br>`);
   document.writeln(`</div>`);
 }
+const image = urlParams.get('image');
 const title = urlParams.get('title');
 const subtitle = urlParams.get('subtitle');
 const description = urlParams.get('desc');
@@ -17,4 +18,4 @@ if (!title || !subtitle || !description || !color){
   alert('A value was not found, stopping rendering...')
   window.stop();
 }
-draw(title, subtitle, marked(description), color);
+draw(image, title, subtitle, marked(description), color);
