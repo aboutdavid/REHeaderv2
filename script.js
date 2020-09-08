71192a0a61cbbae66d4a829f4186ba201f219eb8
Glitch (reheaderv2) <none>
2020-09-08 14:27:47 +0000

    Rewound to commit f45d75a0c47a000c63e84dfc1ce94ceee1feb379 + 0 edits

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-
-
 const urlParams = new URLSearchParams(window.location.search);
-function draw(image, title, subtitle, desc, color){
+function draw(title, subtitle, desc, color){
   document.writeln(`<div style="background-color:${color};"><br><br>`);
-  if (image){
-    document.writeln(`<center><img src="${image}" alt="Profile" width="500" height="600"></center>`)
-  }
   document.writeln(`<p class="desc">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${title}</p>`);
   document.writeln(`<p class="head">&nbsp;&nbsp;&nbsp;&nbsp;${subtitle}</p>`);
-    document.writeln(`<p class="head"><center>&nbsp;&nbsp;&nbsp;&nbsp;${desc}</center></p>`);
   document.writeln(`</div>`);
 }
-const image = urlParams.get('image');
 const title = urlParams.get('title');
 const subtitle = urlParams.get('subtitle');
 const description = urlParams.get('desc');
 const color = urlParams.get('color');
+
+if (!title || !subtitle || !description || !color){
+  alert('A value was not found, stopping rendering...')
+  window.stop();
+}
 draw(title, subtitle, description, color);
