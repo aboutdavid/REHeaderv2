11755ef474a7ee5e80dde5cccc93b0ce073b6349
Glitch (hello-webpage) <none>
2020-09-08 01:26:17 +0000

    🎾📈 Checkpoint ./script.js:8045597/294 ./index.html:8045597/389 ./style.css:8045597/128

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function draw(){
-// 
-  
-  
+// Set form values
+var title = document.getElementById("title").value;
+var subtitle = document.getElementById("subtitle").value;
+var desc = document.getElementById("desc").value
+// Draw canvas 
 var c = document.getElementById("preview");
 var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
