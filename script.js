bc5843d848566b1dd0280f82593e42de1265c07a
Glitch (reheaderv2) <none>
2020-09-08 18:04:35 +0000

    🎿💿 Checkpoint ./script.js:8045597/462 ./form.html:8045597/6246

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ function draw(title, subtitle, desc, color){
   document.writeln(`<p class="head">&nbsp;&nbsp;&nbsp;&nbsp;${subtitle}</p>`);
   document.writeln(`<p style="font-weight:300;font-size:20px;"><center>${desc}</center></p><br>`);
   document.writeln(`</div>`);
-  
-  
+}
+function code(title, subtitle, desc, color){
+  document.writeln(`<textarea><div style="background-color:${color};line-height: 20px;"><br><br>`);
+  document.writeln(`<p class="desc">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${title}</p>`);
+  document.writeln(`<p class="head">&nbsp;&nbsp;&nbsp;&nbsp;${subtitle}</p>`);
+  document.writeln(`<p style="font-weight:300;font-size:20px;"><center>${desc}</center></p><br>`);
+  document.writeln(`</div></textarea>`);
 }
 function lineBreakCount(str){
 	/* counts \n */
