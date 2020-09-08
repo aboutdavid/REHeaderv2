17762f7e230f2cd45f245f06ff7abd44a3b096ac
Glitch (reheaderv2) <none>
2020-09-08 16:37:26 +0000

    🚍📹 Checkpoint ./form.html:8045597/289 ./script.js:8045597/32

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function draw(title, subtitle, desc, color){
   document.writeln(`<div style="background-color:${color};line-height: 20px;"><br><br>`);
   document.writeln(`<p class="desc">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${title}</p>`);
   document.writeln(`<p class="head">&nbsp;&nbsp;&nbsp;&nbsp;${subtitle}</p>`);
-  document.writeln(`<p style="font-weight:300;font-size:20px;"><center>&nbsp;&nbsp;&nbsp;&nbsp;${desc}</center></p><br>`);
+  document.writeln(`<p style="font-weight:300;font-size:20px;"><center>${desc}</center></p><br>`);
   document.writeln(`</div>`);
 }
 const title = urlParams.get('title');
