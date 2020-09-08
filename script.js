9fb9cf68bf21103950e10d4adbdcc02d39a3c026
Glitch (hello-webpage) <none>
2020-09-08 03:05:30 +0000

    🏃🌑 Checkpoint ./index.html:8045597/632 ./form.js:8045597/27 ./form.html:8045597/1962 ./script.js:8045597/740

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const urlParams = new URLSearchParams(window.location.search);
 function draw(title, subtitle, desc, color){
   document.getElementById("preview").innerHTML = `<div style="background-color:lightblue;"><div class="container"><h1>${title}</h1></div></div>`;
 }
+const title = urlParams.get('title');
+const subtitle = urlParams.get('subtitle');
+const description = urlParams.get('desc');
+
+draw(title, subtitle, description)
