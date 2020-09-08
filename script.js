77c2a5bceee51a263508eb5893cb39fd3dc9c20e
Glitch (hello-webpage) <none>
2020-09-08 13:28:46 +0000

    📊😅 Checkpoint ./script.js:8045597/121 ./style.css:8045597/42

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
 function draw(title, subtitle, desc, color){
-  document.writeln(`<div style="background-color:lightblue;"><div class="container-sm"><br><br>`)
-  document.writeln(`<p class="sub">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${title}</p>`)
-  document.writeln(`</div></div>`)
+  document.writeln(`<div style="background-color:lightblue;"><div class="container-sm"><br><br>`);
+  document.writeln(`<p class="sub">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${title}</p>`);
+  document.writeln(`<p class="head">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${subtitle}</p>`);
+  document.writeln(`</div></div>`);
 }
 const title = urlParams.get('title');
 const subtitle = urlParams.get('subtitle');
 const description = urlParams.get('desc');
 
-draw(title, subtitle, description)
+draw(title, subtitle, description);
