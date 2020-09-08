851e3be80c7a8921fc42aed9d52fd224eb98066d
Glitch (reheaderv2) <none>
2020-09-08 14:39:42 +0000

    🦂⚡️ Checkpoint ./script.js:8045597/811 ./index.html:8045597/81 ./form.html:8045597/452

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
-function draw(title, subtitle, desc, color){
+function draw(image, title, subtitle, desc, color){
   document.writeln(`<div style="background-color:${color};"><br><br>`);
+  // <img src="Error.src" onerror="this.style.display='none'"/>
+  document.writeln(`<center><img src="${image}" alt="Profile Picture" width="500" height="500" onerror="this.style.display='none'"></center>`)
   document.writeln(`<p class="desc">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${title}</p>`);
   document.writeln(`<p class="head">&nbsp;&nbsp;&nbsp;&nbsp;${subtitle}</p>`);
+  document.writeln(`<p style="font-weight:300;font-size:20px;"><center>&nbsp;&nbsp;&nbsp;&nbsp;${desc}</center></p>`);
   document.writeln(`</div>`);
 }
 const title = urlParams.get('title');
@@ -14,4 +17,4 @@ if (!title || !subtitle || !description || !color){
   alert('A value was not found, stopping rendering...')
   window.stop();
 }
-draw(title, subtitle, description, color);
+draw(title, subtitle, marked(description), color);
